af6ddafcc20a05d5f83dc1b9fa7913d77d2bd36a
istiaqmonjur70-create <istiaqmonjur70@gmail.com>
2026-09-21 20:52:09 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def keep_alive():
     print("Flask Keep-Alive server started.")
 
 # --- Configuration ---
-TOKEN = os.environ.get("BOT_TOKEN", "8910223271:AAG4U3fDLzI8ONc5Wns8VMmXXyuXYyrp1d4").strip()
+TOKEN = os.environ.get("BOT_TOKEN", "8910223271:AAF4edYH15Mdn5uGHQpHi3VXSKS7NLe90IU").strip()
 OWNER_ID = 8814363793
 ADMIN_ID = 8814363793
 YOUR_USERNAME = "@DevCloudX"
@@ -3739,7 +3739,7 @@ def handle_text_messages(message):
 # Keep these values after the main code as requested.
 # Replace only the two placeholders below.
 # =====================================================================
-SECOND_BOT_TOKEN = os.environ.get("SECOND_BOT_TOKEN", 8910223271:AAG4U3fDLzI8ONc5Wns8VMmXXyuXYyrp1d4").strip()
+SECOND_BOT_TOKEN = os.environ.get("SECOND_BOT_TOKEN", "8910223271:AAF4edYH15Mdn5uGHQpHi3VXSKS7NLe90IU").strip()
 SECOND_ADMIN_ID = 8814363793
 
 APPROVAL_ADMIN_IDS = {int(OWNER_ID), int(ADMIN_ID)}
